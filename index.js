2353b3611e21982a84d568a6dd696e03b6b8d8f0
himalay03 <114144373+himalay03@users.noreply.github.com>
2023-04-08 22:28:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-// const http = require("http");
 
-// const server = http.createServer((req, res) => {
-//   if ((req.url = "/")) {
-//     res.end("Hello from this side");
-//   } else if ((req.url = "/userapi")) {
-//     res.end("Hello from the userapi side");
-//   }
-// });
-
-// server.listen(8080, "127.0.0.1", () => {
-//   console.log("listening to the port no. 8080");
-// });
 const express = require("express");
 const app = express();
-// const data = express("./userapi.json");
 const fs = require("fs");
 
 app.get("/", (req, res) => {
@@ -22,7 +9,6 @@ app.get("/", (req, res) => {
 });
 
 app.get("/userapi", (req, res) => {
-  //   res.json(data);
   fs.readFile("./userapi.json", "utf-8", (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
